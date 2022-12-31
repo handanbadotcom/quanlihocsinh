940d261af3befeaaadcf2ea0d477cd9b0defd97c
thanhtrietla <95730098+thanhtrietla@users.noreply.github.com>
2022-12-31 21:58:57 +0700

    Tra cứu, Nhận bảng điểm

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import LOPHOC, HOCSINH
+from .models import LOPHOC, HOCSINH, Subject, Grade
 # Register your models here.
 
 admin.site.register(LOPHOC)
 admin.site.register(HOCSINH)
+admin.site.register(Subject)
+admin.site.register(Grade)
