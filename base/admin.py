026feee6a8b9e5c6f34c5a26d48ec14afb932f5d
handanbadotcom <cd.synguyen@gmail.com>
2022-12-31 22:26:28 +0700

    sửa scrolll bar

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import LOPHOC, HOCSINH
+from .models import LOPHOC, HOCSINH, Age
 # Register your models here.
 
 admin.site.register(LOPHOC)
 admin.site.register(HOCSINH)
+admin.site.register(Age)
