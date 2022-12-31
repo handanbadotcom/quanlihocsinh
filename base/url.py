1a28fa5959467fc6dc51ceea56d2590b7819f728
handanbadotcom <cd.synguyen@gmail.com>
2022-12-31 23:00:06 +0700

    sửa giao diện

--- base/url.py
+++ base/url.py
@@ -8,8 +8,7 @@ urlpatterns = [
     path('',views.home,name = 'home'),
     path('tiepnhanhs/', views.TiepnhanHS, name = 'TiepnhanHS'),
     path('search-student', views.searchStudent, name='search'),
-    path('receive-transcripts', views.receiveTranscripts, name='transcripts')
-
+    path('receive-transcripts', views.receiveTranscripts, name='transcripts'),
     path("quanlidotuoi/", views.quanlidotuoi, name='quanlidotuoi'),
     path("quanlidotuoi/capnhat/<int:age_id>", views.capNhatTuoi, name='capNhatTuoi'),
     path("quanlidotuoi/delete/<int:age_id>", views.xoaTuoi, name='xoaTuoi'),
