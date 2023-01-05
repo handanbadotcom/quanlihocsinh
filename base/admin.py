5c313e6376a8e46304afdf1645207dc4ba53be8b
handanbadotcom <cd.synguyen@gmail.com>
2023-01-05 23:53:05 +0700

    merge

--- base/admin.py
+++ base/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import LOPHOC, HOCSINH, Subject, Grade, Age
+from .models import LOPHOC, HOCSINH, MONHOC, Grade, Age
 # Register your models here.
 
 admin.site.register(LOPHOC)
 admin.site.register(HOCSINH)
 admin.site.register(Age)
-admin.site.register(Subject)
+admin.site.register(MONHOC)
 admin.site.register(Grade)
