401676b0fa0e325b42f590d63586b49b21c33ddf
thanhtrietla <thanhtriet2411@gmail.com>
2023-01-06 15:48:51 +0700

    Handle exceptions

--- base/views.py
+++ base/views.py
@@ -91,18 +91,21 @@ def searchStudent(request):
         while True:
             if name == '' or className == '':
                 message = 'Vui lòng nhập đầy đủ thông tin!'
+                print(message)
                 break
             
             try:
                 classRoom = LOPHOC.objects.get(TENLOP=className)
             except:
                 message = 'Lớp học không tồn tại!'
+                print(message)
                 break
             
             try:
                 student = HOCSINH.objects.get(HOTEN=name, LOPHOC=classRoom)
             except:
                 message = 'Học sinh không tồn tại!'
+                print(message)
                 break
             
             i = 1
@@ -116,6 +119,25 @@ def searchStudent(request):
                 avg.append(round(sum(iSemesterAVGs)/len(iSemesterAVGs), 1))
             break
 
+<<<<<<< HEAD
+=======
+        classRoom = LOPHOC.objects.get(TENLOP=className)
+        try:
+            student = HOCSINH.objects.get(HOTEN=name, LOPHOC=classRoom)
+        except:
+            message = 'Student does not exist!'
+            print(message)
+        i = 1
+        while i <= 2:
+            iSemesterGrades = Grade.objects.filter(student=student, semester=i)
+            print(iSemesterGrades)
+            iSemesterAVGs = []
+            i+=1
+            for subject in iSemesterGrades:
+                iSemesterAVGs.append(subject.AVG)
+            avg.append(round(sum(iSemesterAVGs)/len(iSemesterAVGs), 1))
+    
+>>>>>>> 71a4a0431289b4d28c844653cf151ec36b21324f
     context = {'student': student, 'message': message, 'avg': avg}
     return render(request, 'base/search_student.html', context)
 
@@ -154,6 +176,9 @@ def receiveTranscripts(request):
                 message = 'Môn học không tồn tại!'
                 print(message)
                 break
+                
+            students = HOCSINH.objects.filter(LOPHOC=classRoom)
+            print(students)
             
             students = HOCSINH.objects.filter(LOPHOC=classRoom)
             
